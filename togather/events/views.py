58e6e657a1c1ae941e09d0e8325f9b517ac43064
Clarence <yunfan.jiang@emory.edu>
2021-10-17 10:15:12 -0400

    update api view

--- togather/events/views.py
+++ togather/events/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+# from django.shortcuts import render     # Clarence's adjustment starts here
+from django.http import HttpResponse
 
-# Create your views here.
 from .models import Event, User_special
 from .serializers import EventSerializer, UserSerializer
 from rest_framework import viewsets
@@ -8,6 +8,9 @@ from rest_framework import permissions
 from django.contrib.auth.models import User
 
 
+# Create your views here.
+def index(request):
+    return HttpResponse("API View")
 
 
 class EventViewSet(viewsets.ModelViewSet):
